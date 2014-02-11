33474f411d928592f883faee1f90f307596de945
Michael Christenson II <michael@rebelhold.com>
2014-02-10 22:16:09 -0700

    added a sketch of a test so that travis-ci had something to work on

---
+++ test/unit/user.js
@@ -0,0 +1,9 @@
+var chai   = require('chai'),
+    assert = require('assert');
+
+describe('User', function () {
+  describe('#save', function () {
+    it('creates a user if id is null');
+    it('updates a user if id is not null');
+  });
+});
